681407f70ed5ab7890a16c7974031e3ce03fd95c
Michel <michel.zanona@gmail.com>
2025-01-27 17:40:50 +0100

    creazione

---
+++ src/test/java/com/betacom/terme/FinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.betacom.terme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
