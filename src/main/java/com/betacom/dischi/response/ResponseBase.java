fa273e2314877781cbf88ed3722f558ddc96542d
Michel <michel.zanona@gmail.com>
2025-01-29 16:25:20 +0100

    copia response

---
+++ src/main/java/com/betacom/dischi/response/ResponseBase.java
@@ -0,0 +1,23 @@
+package com.betacom.dischi.response;
+
+public class ResponseBase {
+
+	private Boolean rC;
+	private String msg;
+	
+	public Boolean getrC() {
+		return rC;
+	}
+	public void setrC(Boolean rC) {
+		this.rC = rC;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/betacom/dischi/response/ResponseObject.java
@@ -0,0 +1,15 @@
+package com.betacom.dischi.response;
+
+public class ResponseObject<T> extends ResponseBase {
+
+	private T dati;
+
+	public T getDati() {
+		return dati;
+	}
+
+	public void setDati(T dati) {
+		this.dati = dati;
+	}
+	
+}
