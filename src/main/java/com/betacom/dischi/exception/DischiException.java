e0b6c2bd2f545d140aa7e5c219563f423e5af3d8
Michel <michel.zanona@gmail.com>
2025-01-29 20:15:15 +0100

    aggiunto Exception e .properties

---
+++ src/main/java/com/betacom/dischi/exception/DischiException.java
@@ -0,0 +1,15 @@
+package com.betacom.dischi.exception;
+
+public class DischiException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public DischiException() {
+		super();
+	}
+	
+	public DischiException(String msg) {
+		super(msg);
+	}
+	
+}
---
+++ src/main/java/com/betacom/dischi/utilities/Utility.java
@@ -0,0 +1,5 @@
+package com.betacom.dischi.utilities;
+
+public class Utility {
+
+}
