ed80dfc105e04a6b8020ee58ef964ea32403df47
riccardo98b <riccardobelloni98@gmail.com>
2025-01-30 18:10:45 +0100

    Recensione Repository

---
+++ src/main/java/com/betacom/dischi/repository/IRecensioneRepository.java
@@ -0,0 +1,9 @@
+package com.betacom.dischi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.betacom.dischi.models.Recensione;
+
+public interface IRecensioneRepository extends JpaRepository<Recensione,Integer>  {
+
+}
