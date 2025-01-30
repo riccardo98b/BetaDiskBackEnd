ee0f319ef0701b43afc54062c953ac257d51444c
Michel <michel.zanona@gmail.com>
2025-01-30 17:24:57 +0100

    correzione

--- src/main/java/com/betacom/dischi/services/implementations/ClienteImpl.java
+++ src/main/java/com/betacom/dischi/services/implementations/ClienteImpl.java
@@ -2,10 +2,13 @@ package com.betacom.dischi.services.implementations;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.betacom.dischi.DTO.ClienteDTO;
 import com.betacom.dischi.request.ClienteRequest;
 import com.betacom.dischi.services.interfaces.ClienteService;
 
+@Service
 public class ClienteImpl implements ClienteService {
 
 	@Override
