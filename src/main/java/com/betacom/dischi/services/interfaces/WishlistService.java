da3beec2ecf4ba9993dd03a8ea5751b6bd196552
Yohannes Luminari <67595372+yohannesluminari@users.noreply.github.com>
2025-01-30 17:29:32 +0100

    wisth service

--- src/main/java/com/betacom/dischi/services/interfaces/WishlistService.java
+++ src/main/java/com/betacom/dischi/services/interfaces/WishlistService.java
@@ -1,5 +1,21 @@
 package com.betacom.dischi.services.interfaces;
 
+import java.util.List;
+
+import com.betacom.dischi.DTO.WishlistDTO;
+import com.betacom.dischi.request.WishlistRequest;
+
 public interface WishlistService {
 
+List<WishlistDTO> listAll();	
+	
+
+	
+	void create(WishlistRequest req) throws Exception;
+		
+	 void update(WishlistRequest req) throws Exception;
+	    
+	    
+    
+	void delete(WishlistRequest req) throws Exception;
 }
