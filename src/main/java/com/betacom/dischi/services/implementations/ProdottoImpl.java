56bbc5b3b5c78574c6a7b198419afc105def6c2e
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2025-01-30 18:12:11 +0100

    add methods in ProdottoService & Implementations

--- src/main/java/com/betacom/dischi/services/implementations/ProdottoImpl.java
+++ src/main/java/com/betacom/dischi/services/implementations/ProdottoImpl.java
@@ -1,11 +1,58 @@
 package com.betacom.dischi.services.implementations;
 
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.betacom.dischi.DTO.ProdottoDTO;
+import com.betacom.dischi.exception.CustomException;
+import com.betacom.dischi.models.Prodotto;
+import com.betacom.dischi.repository.IProdottoRepository;
+import com.betacom.dischi.request.ProdottoRequest;
 import com.betacom.dischi.services.interfaces.ProdottoService;
 
 @Service
 public class ProdottoImpl implements ProdottoService{
+	
+	@Autowired
+	Logger log;
+	
+	@Autowired
+	IProdottoRepository prodottoRepository;
+	
+	
+
+	@Override
+	public void create(ProdottoRequest req) throws Exception {
+	
+		Prodotto prodotto = new Prodotto();
+		
+		if(req.getFormato() == null)
+			throw new CustomException("Inserisci il formato del prodotto");
+		
+			
+		
+	}
+
+	@Override
+	public List<ProdottoDTO> listAll() {
+		
+		return null;
+	}
+
+	@Override
+	public void update(ProdottoRequest req) throws Exception {
+
+		
+	}
+
+	@Override
+	public void delete(ProdottoRequest req) throws Exception {
+
+		
+	}
 	
 
 }
--- src/main/java/com/betacom/dischi/services/interfaces/ProdottoService.java
+++ src/main/java/com/betacom/dischi/services/interfaces/ProdottoService.java
@@ -1,5 +1,18 @@
 package com.betacom.dischi.services.interfaces;
 
+import java.util.List;
+
+import com.betacom.dischi.DTO.ProdottoDTO;
+import com.betacom.dischi.request.ProdottoRequest;
+
 public interface ProdottoService {
 
+	void create(ProdottoRequest req)throws Exception;
+	
+	List<ProdottoDTO> listAll();
+	
+	void update(ProdottoRequest req)throws Exception;
+	
+	void delete(ProdottoRequest req)throws Exception;
+	
 }
