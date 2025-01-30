e0f5aea368da7108105b09642b200c87795671c6
riccardo98b <riccardobelloni98@gmail.com>
2025-01-30 17:53:25 +0100

    Creazione repository cliente e recensione

---
+++ src/main/java/com/betacom/dischi/repository/IClienteRepository.java
@@ -0,0 +1,9 @@
+package com.betacom.dischi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.betacom.dischi.models.Cliente;
+
+public interface IClienteRepository extends JpaRepository<Cliente, Integer> {
+
+}
