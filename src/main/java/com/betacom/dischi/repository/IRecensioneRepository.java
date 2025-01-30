0e5ae46dc383fa4086075cea8dad7c16caac8854
riccardo98b <riccardobelloni98@gmail.com>
2025-01-30 18:08:48 +0100

    Cliente Repository

--- src/main/java/com/betacom/dischi/repository/IRecensioneRepository.java
+++
@@ -1,9 +0,0 @@
-package com.betacom.dischi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.betacom.dischi.models.Recensione;
-
-public interface IRecensioneRepository extends JpaRepository<Recensione, Integer> {
-
-}
