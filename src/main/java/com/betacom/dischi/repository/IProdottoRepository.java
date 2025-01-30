26eb943c689ecb6f67ecfb0e0a0488c101e902ba
AlessiaLuongo <151174462+AlessiaLuongo@users.noreply.github.com>
2025-01-30 17:19:15 +0100

    creazione  Prodotto -> Controller, Repository, Implementation & Service

---
+++ src/main/java/com/betacom/dischi/repository/IProdottoRepository.java
@@ -0,0 +1,9 @@
+package com.betacom.dischi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.betacom.dischi.models.Prodotto;
+
+public interface IProdottoRepository extends JpaRepository<Prodotto, Integer>{
+
+}
