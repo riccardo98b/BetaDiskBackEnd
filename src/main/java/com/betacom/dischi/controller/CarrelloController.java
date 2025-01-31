f8081316c9da3032ba65501470520a5e8ab2a5bf
Michel <michel.zanona@gmail.com>
2025-01-31 18:03:01 +0100

    aggiornato RC

--- src/main/java/com/betacom/dischi/controller/CarrelloController.java
+++ src/main/java/com/betacom/dischi/controller/CarrelloController.java
@@ -7,10 +7,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.betacom.dischi.DTO.CarrelloDTO;
 import com.betacom.dischi.exception.CustomException;
 import com.betacom.dischi.request.CarrelloRequest;
 import com.betacom.dischi.request.ProdottoRequest;
 import com.betacom.dischi.response.ResponseBase;
+import com.betacom.dischi.response.ResponseList;
 import com.betacom.dischi.services.interfaces.CarrelloService;
 
 @RestController
@@ -24,13 +26,14 @@ public class CarrelloController {
 	CarrelloService carrelloServ;
 	
 	@PostMapping("/add")
-	public ResponseBase addProdotto(@RequestBody(required = true) CarrelloRequest request, @RequestBody ProdottoRequest prodRequest) {
+	public ResponseList<CarrelloDTO> addProdotto(@RequestBody(required = true) CarrelloRequest request, @RequestBody ProdottoRequest prodRequest) {
 		log.debug("Aggiungi prodotto: " + prodRequest);
-		ResponseBase response = new ResponseBase();
+		ResponseList<CarrelloDTO> response = new ResponseList<CarrelloDTO>();
 		response.setRc(true);
 		try {
 			carrelloServ.addProdotto(request, prodRequest);
 			response.setMsg("Prodotto aggiunto con successo");
+			response.setDati(carrelloServ.listaProdotti(request.getIdCliente()));
 		} catch (CustomException e) {
 			log.error(e.getMessage());
 			response.setRc(false);
