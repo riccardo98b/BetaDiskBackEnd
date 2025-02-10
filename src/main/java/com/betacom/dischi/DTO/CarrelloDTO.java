3423a661d1a275c63c5e450e327bb56f940f741a
Michel <michel.zanona@gmail.com>
2025-02-10 17:28:00 +0100

    BULD!

--- src/main/java/com/betacom/dischi/DTO/CarrelloDTO.java
+++ src/main/java/com/betacom/dischi/DTO/CarrelloDTO.java
@@ -2,6 +2,8 @@ package com.betacom.dischi.DTO;
 
 import java.util.List;
 
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties.Build;
+
 import com.betacom.dischi.DTO.ClienteDTO;
 import com.betacom.dischi.DTO.ProdottoDTO;
 
@@ -12,53 +14,62 @@ public class CarrelloDTO {
 	private List<ProdottoDTO> prodotti;
 	private ClienteDTO cliente;
 	private Integer quantita;
-	public CarrelloDTO() {
+	private CarrelloDTO() {
 		super();
-	}
-	public CarrelloDTO(Integer idCarrello, Double totale, List<ProdottoDTO> prodotti, ClienteDTO cliente, Integer quantita) {
-		super();
-		this.idCarrello = idCarrello;
-		this.totale = totale;
-		this.prodotti = prodotti;
-		this.cliente = cliente;
-		this.quantita = quantita;
-	}
+	}	
 	public Integer getIdCarrello() {
 		return idCarrello;
-	}
-	public void setIdCarrello(Integer idCarrello) {
-		this.idCarrello = idCarrello;
 	}
 	public Double getTotale() {
 		return totale;
 	}
-	public void setTotale(Double totale) {
-		this.totale = totale;
-	}
 	public List<ProdottoDTO> getProdotti() {
 		return prodotti;
-	}
-	public void setProdotti(List<ProdottoDTO> prodotti) {
-		this.prodotti = prodotti;
 	}
 	public ClienteDTO getCliente() {
 		return cliente;
 	}
-	public void setCliente(ClienteDTO cliente) {
-		this.cliente = cliente;
-	}
 	public Integer getQuantita() {
 		return quantita;
 	}
-	public void setQuantita(Integer quantita) {
-		this.quantita = quantita;
-	}
-	@Override
-	public String toString() {
-		return "CarrelloDTO [idCarrello=" + idCarrello + ", totale=" + totale + ", prodotti=" + prodotti + ", cliente="
-				+ cliente + ", quantita=" + quantita + ", getIdCarrello()=" + getIdCarrello() + ", getTotale()="
-				+ getTotale() + ", getProdotti()=" + getProdotti() + ", getCliente()=" + getCliente()
-				+ ", getQuantita()=" + getQuantita() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode()
-				+ ", toString()=" + super.toString() + "]";
-	}
+	
+	public static class Builder {
+		private Integer idCarrello;
+		private Double totale;
+		private List<ProdottoDTO> prodotti;
+		private ClienteDTO cliente;
+		private Integer quantita;
+		public Builder() {
+		}
+		public CarrelloDTO build() {
+			CarrelloDTO dto = new CarrelloDTO();
+			dto.idCarrello = this.idCarrello;
+			dto.totale = this.totale;
+			dto.prodotti = this.prodotti;
+			dto.cliente = this.cliente;
+			dto.quantita = this.quantita;
+			return dto;
+		}
+		public Builder idCarrello(Integer idCarrello) {
+			this.idCarrello = idCarrello;
+			return this;
+		}
+		public Builder totale(Double totale) {
+			this.totale = totale;
+			return this;
+		}
+		public Builder prodotti(List<ProdottoDTO> prodotti) {
+			this.prodotti = prodotti;
+			return this;
+		}
+		public Builder cliente(ClienteDTO cliente) {
+			this.cliente = cliente;
+			return this;
+		}
+		public Builder quantita(Integer quantita) {
+			this.quantita = quantita;
+			return this;
+		}
+	}	
+	
 }
