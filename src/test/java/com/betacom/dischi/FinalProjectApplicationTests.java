1bd539e377e79259441e337f8a7388c3e2cd562d
Michel <michel.zanona@gmail.com>
2025-02-11 18:05:13 +0100

    creazione suite test

--- src/test/java/com/betacom/dischi/FinalProjectApplicationTests.java
+++ src/test/java/com/betacom/dischi/FinalProjectApplicationTests.java
@@ -1,7 +1,19 @@
 package com.betacom.dischi;
 
 import org.junit.jupiter.api.Test;
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
+
+@Suite
+@SelectClasses({
+	ProdottoControllerTest.class,
+	ClienteControllerTest.class,
+	CarrelloControllerTest.class,
+	OrdineControllerTest.class,
+	RecensioneControllerTest.class
+	
+})
 
 @SpringBootTest
 class FinalProjectApplicationTests {
