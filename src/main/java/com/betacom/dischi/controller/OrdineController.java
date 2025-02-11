487c35b6cb92645f39cbc618c086628f43944b01
Michel <michel.zanona@gmail.com>
2025-02-11 17:39:05 +0100

    lista ordini per cliente

--- src/main/java/com/betacom/dischi/controller/OrdineController.java
+++ src/main/java/com/betacom/dischi/controller/OrdineController.java
@@ -2,14 +2,20 @@ package com.betacom.dischi.controller;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.betacom.dischi.DTO.CarrelloDTO;
+import com.betacom.dischi.DTO.OrdineDTO;
 import com.betacom.dischi.exception.CustomException;
 import com.betacom.dischi.request.OrdineRequest;
 import com.betacom.dischi.response.ResponseBase;
+import com.betacom.dischi.response.ResponseList;
+import com.betacom.dischi.response.ResponseObject;
 import com.betacom.dischi.services.interfaces.OrdineService;
 
 @RestController
@@ -69,6 +75,22 @@ public class OrdineController {
 			response.setRc(false);
 			response.setMsg(e.getMessage());
 		
+		}
+		return response;
+	}
+	
+	@GetMapping("/lista")
+	public ResponseList<OrdineDTO> listaProdotti(@RequestParam Integer id) {
+		log.debug("Lista carrello: " + id);
+		ResponseList<OrdineDTO> response = new ResponseList<OrdineDTO>();
+		response.setRc(true);
+		try {	
+			response.setMsg("Ordini effettuati");
+			response.setDati(ordineServ.listaByCliente(id));
+		} catch (CustomException e) {
+			log.error(e.getMessage());
+			response.setRc(false);
+			response.setMsg(e.getMessage());
 		}
 		return response;
 	}
