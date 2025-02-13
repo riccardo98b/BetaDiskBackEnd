7bf5cb8d5c9aa81940c006d25468c4398c22dcae
Michel <michel.zanona@gmail.com>
2025-02-13 10:02:03 +0100

    crossorigin su controller carrello/ordine

--- src/main/java/com/betacom/dischi/controller/OrdineController.java
+++ src/main/java/com/betacom/dischi/controller/OrdineController.java
@@ -2,6 +2,7 @@ package com.betacom.dischi.controller;
 
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +19,7 @@ import com.betacom.dischi.response.ResponseList;
 import com.betacom.dischi.response.ResponseObject;
 import com.betacom.dischi.services.interfaces.OrdineService;
 
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/rest/ordine")
 public class OrdineController {
