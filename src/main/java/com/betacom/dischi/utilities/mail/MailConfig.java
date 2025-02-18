a2c5c1cab72ff3196c47cd5c8decdfbb10b76eed
Michel <michel.zanona@gmail.com>
2025-02-18 10:22:59 +0100

    variabili d'ambiente in mail

--- src/main/java/com/betacom/dischi/utilities/mail/MailConfig.java
+++ src/main/java/com/betacom/dischi/utilities/mail/MailConfig.java
@@ -2,6 +2,7 @@ package com.betacom.dischi.utilities.mail;
 
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -9,22 +10,27 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
 public class MailConfig {
+	
+	@Value("${email}")
+	private String username;
+	@Value("${googlepass}")
+	private String googlePass;
 
-	 @Bean
-	 JavaMailSender getJavaMailSender() {
-		 JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-	     mailSender.setHost("smtp.gmail.com");
-	     mailSender.setPort(587);
+	@Bean
+	JavaMailSender getJavaMailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+	    mailSender.setHost("smtp.gmail.com");
+	    mailSender.setPort(587);
 
-	     mailSender.setUsername("rimosso@rimosso.com");
-	     mailSender.setPassword("glwl drjk lmda xvdd");
+	    mailSender.setUsername(username);
+	    mailSender.setPassword(googlePass.trim());
 
-	     Properties props = mailSender.getJavaMailProperties();
-	     props.put("mail.transport.protocol", "smtp");
-	     props.put("mail.smtp.auth", "true");
-	     props.put("mail.smtp.starttls.enable", "true");
-	     props.put("mail.debug", "true");
+	    Properties props = mailSender.getJavaMailProperties();
+	    props.put("mail.transport.protocol", "smtp");
+	    props.put("mail.smtp.auth", "true");
+	    props.put("mail.smtp.starttls.enable", "true");
+	    props.put("mail.debug", "true");
 
-	     return mailSender;
+	    return mailSender;
 	}
 }
