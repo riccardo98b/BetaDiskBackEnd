441802f3ee7ae952097a03dc752a19f9ef652482
Michel <michel.zanona@gmail.com>
2025-02-21 12:39:16 +0100

    Update MailService.java

--- src/main/java/com/betacom/dischi/utilities/mail/MailService.java
+++ src/main/java/com/betacom/dischi/utilities/mail/MailService.java
@@ -1,7 +1,5 @@
 package com.betacom.dischi.utilities.mail;
 
-import java.time.LocalDate;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
